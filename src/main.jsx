4d01f67723fa5156755d41a4bc56c1dacc0053da
Naufal453 <alamsyahnaufal453@gmail.com>
2026-03-11 18:15:06 +0700

    fix font bundling

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+
+// Local fonts bundling
+import "@fontsource/syne/400.css";
+import "@fontsource/syne/700.css";
+import "@fontsource/syne/800.css";
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/jetbrains-mono/400.css";
+import "@fontsource/jetbrains-mono/500.css";
+
 import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
